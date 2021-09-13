a1a3c7f1307b92d0d6c1f0d2489a00ffe9cee5db
OisinPegman <oisin.pegman@gmail.com>
2021-09-13 17:07:34 +0100

    Added code to manage banning and banned viewers.
    Added File to hold alt names. added code to add to banned list. added code to check if viewer is on ban list. added separate mod commands function.

--- server.js
+++ server.js
@@ -22,6 +22,18 @@ const client = new tmi.client(options);
 
 const CMDstart = process.env.COMMAND_START;
 
+var Alts;
+
+const ViewersURL = `https://tmi.twitch.tv/group/user/${process.env.TARGET_CHANNEL}/chatters`;
+
+try {
+    const jsonAlts = fs.readFileSync('NameAlts.json');
+    Alts = JSON.parse(jsonAlts);
+} catch (err) {
+    console.log(err);
+    return;
+}
+
 client.connect();
 
 client.on('connected', ( address, port ) => {
@@ -30,6 +42,16 @@ client.on('connected', ( address, port ) => {
 
 client.on('join', (channel, username, self) => {
     if (self) return;
+
+    tempNames = [];
+
+    username.forEach(user => {
+        if (IsBanned(username))
+            tempNames.push(user);
+    });
+
+    if (tempNames.length > 0)
+        client.say(channel, `@OPlonker1 There are ${tempNames.length} possible ban bypassers.\n ${tempNames}`);
 
     console.log(username);
 });
@@ -47,20 +69,29 @@ function CommandHandler(channel, tags, message) {
 	const args = message.slice( CMDstart.length ).split(' ');
 	const command = args.shift().toLowerCase();
 
-	if(command === 'echo') {
-		client.say(channel, `@${tags.username}, you said: "${args.join(' ')}"`);
-    } else if (command === 'addban') {
+    ModCommandHandler(channel, tags, command, args);
+
+    if (command === 'echo') {
+        client.say(channel, `@${tags.username}, you said: "${args.join(' ')}"`);
+    } 
+}
+
+function ModCommandHandler(channel, tags, command, args) {
+    if (tags.mod !== true) return;
+
+    if (command === 'addban') {
         if (args.length == 0) return;
 
         user = args.shift().toLowerCase();
+        if (IsBanned(user)) return;
 
         if (args.length != 0)
             reason = args.join(' ');
         else
             reason = '';
         
-        BanHandler(user, reason);
-    }
+        AddBan(user, reason);
+    } 
 }
 
 function MessageHandler(channel, tags, message) {
@@ -70,6 +101,36 @@ function MessageHandler(channel, tags, message) {
         client.say(channel, `@${tags.username} modCheck? Do you really want to incur the wrath of the all powerful mods?`);
 }
 
-function BanHandler(user, reason) {
+function AddBan(user, reason) {
+    if (IsBanned(user)) return;
     
+    const alt = { BanReason: reason, AccountAlts: GenerateAlts(user) };   
+
+    Alts.push(alt);
+
+    fs.writeFileSync('NameAlts.json', JSON.stringify(Alts), err => {
+        if (err)
+            console.log('Error writing file', err)
+    });
+}
+function IsBanned(username) {
+    if (Alts.length === 0) return false;
+    
+    let val = false;
+
+    Alts.forEach(user => {
+        if (user.AccountAlts.indexOf(username) != -1) {
+            val =  true;
+        }
+    });
+
+    return val;
+}
+
+function GenerateAlts(user) {
+    altNames = [user];
+
+
+    console.log(`${altNames.length} alts found for ${user}.`);
+    return altNames;
 }
