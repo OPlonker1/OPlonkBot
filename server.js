e24a63330f3d41ed43bc779102c1fb7279e51643
OisinPegman <oisin.pegman@gmail.com>
2021-09-13 18:44:19 +0100

    Added Leets. Fixed Bug in on join.

--- server.js
+++ server.js
@@ -24,6 +24,45 @@ const CMDstart = process.env.COMMAND_START;
 
 var Alts;
 
+const Leets = {
+    'a': ['a', '4'],
+    'b': ['b', '8', '6'],
+    'c': ['c'],
+    'd': ['d'],
+    'e': ['e', '3'],
+    'f': ['f', '7'],
+    'g': ['g', '6'],
+    'h': ['h', '4'],
+    'i': ['i', '1', '9'],
+    'j': ['j', '7', '9'],
+    'k': ['k'],
+    'l': ['l', '1'],
+    'm': ['m'],
+    'n': ['n'],
+    'o': ['o', '0'],
+    'p': ['p'],
+    'q': ['q', '9'],
+    'r': ['r'],
+    's': ['s', '5'],
+    't': ['t', '7'],
+    'u': ['u'],
+    'v': ['v'],
+    'w': ['w'],
+    'x': ['x'],
+    'y': ['y'],
+    'z': ['z', '5'],
+    '0': ['0', 'o'],
+    '1': ['1', 'i', 'l', '7'],
+    '2': ['2', 'z'],
+    '3': ['3', 'e'],
+    '4': ['4', 'h', 'a'],
+    '5': ['5', 's'],
+    '6': ['6', 'b', 'g'],
+    '7': ['7', 't', 'j', 'l'],
+    '8': ['8', 'b'],
+    '9': ['9', 'g']
+};
+
 const ViewersURL = `https://tmi.twitch.tv/group/user/${process.env.TARGET_CHANNEL}/chatters`;
 
 try {
@@ -37,21 +76,14 @@ try {
 client.connect();
 
 client.on('connected', ( address, port ) => {
-    client.action( process.env.TARGET_CHANNEL, 'OPlonkBot has started.' );
+    client.action( process.env.TARGET_CHANNEL, 'OPlonkBot v1.0 has started.' );
 });
 
 client.on('join', (channel, username, self) => {
     if (self) return;
 
-    tempNames = [];
-
-    username.forEach(user => {
-        if (IsBanned(username))
-            tempNames.push(user);
-    });
-
-    if (tempNames.length > 0)
-        client.say(channel, `@OPlonker1 There are ${tempNames.length} possible ban bypassers.\n ${tempNames}`);
+    if (IsBanned(username))
+        client.say(channel, `@OPlonker1 There is a possible ban bypasser.\n ${username}`);
 
     console.log(username);
 });
