123cc20afaab7fc7e7a869eb9fc159782a36e6dd
OisinPegman <oisin.pegman@gmail.com>
2021-09-18 12:49:19 +0100

    Added file to handle the banning processes

---
+++ BanManager.js
@@ -0,0 +1,57 @@
+const AltGenerator = require('./altGenerator');
+const Database = require('./dataManager');
+
+module.exports.Init = Init;
+module.exports.AddToWatchlist = AddToWatchlist;
+module.exports.RmWatchlist = RmWatchlist;
+module.exports.IsBanned = IsBanned;
+module.exports.GetBannedUser = GetBannedUser;
+
+var Alts = null;
+
+function Init() {
+    Alts = Database.Read();
+}
+
+function AddToWatchlist(user, reason) {
+    const alt = { BanReason: reason, AccountAlts: AltGenerator(user) };   
+    Alts.push(alt);
+
+    Database.Write(Alts);
+
+    return alt.AccountAlts.length - 1;
+}
+
+function RmWatchlist(user) {
+    let [isBan, index] = IsBanned(user);
+    
+    if (!isBan) return;
+    Alts.splice(index);
+
+    Database.Write(Alts);
+}
+
+function IsBanned(username) {
+    let banned = false;
+    let arrayIndex = -1;
+
+    if (Alts.length === 0) return [banned, arrayIndex];
+    
+    Alts.forEach(user => {
+        user.AccountAlts.forEach(alt => {
+            if (username.includes(alt)) {
+                banned = true;
+                arrayIndex = Alts.indexOf(user);
+            }
+        })
+    });
+
+    return [banned, arrayIndex];
+}
+
+function GetBannedUser(username) {
+    let [isBanned, banIndex] = IsBanned(username);
+    if (!isBanned) return null;
+
+    return Alts[banIndex];
+}
