8d56a191e24c6a6fc9f91476ca354f8e4f504b70
OisinPegman <oisin.pegman@gmail.com>
2021-10-19 16:15:00 +0100

    Fixed file path error

--- bot/data/DataManager.js
+++ bot/data/DataManager.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 module.exports.Read = Read;
 module.exports.Write = Write;
 
+const currentDirectory = 'bot/data/';
 const fileName = 'WatchList.json';
 
 function Read() {
     try {
-        const jsonAlts = fs.readFileSync(fileName, 'utf8');
+        const jsonAlts = fs.readFileSync(currentDirectory + fileName);
         return JSON.parse(jsonAlts);
     } catch (err) {
         console.log(err);
@@ -17,7 +18,7 @@ function Read() {
 
 function Write(data) {
     let jsonData = JSON.stringify(data, null, 4);
-    fs.writeFileSync(fileName, jsonData, err => {
+    fs.writeFileSync(currentDirectory + fileName, jsonData, err => {
         if (err)
             console.log('Error writing file', err)
     });
