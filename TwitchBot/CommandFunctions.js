08e707fe1dc0a6a8d6bf4847778f8e4157c62cc3
OisinPegman <oisin.pegman@gmail.com>
2021-11-05 16:23:47 +0000

    Clean up Command calling code

--- TwitchBot/CommandFunctions.js
+++ TwitchBot/CommandFunctions.js
@@ -6,13 +6,23 @@ const Viewers = require('./GetViewerList');
 /*** Command Handlers ***/
 
 module.exports.Init = Init;
-module.exports.CommandHandler = CommandHandler;
-module.exports.MessageHandler = MessageHandler;
+module.exports.ChatHandler = ChatHandler;
+
+let MessageFunctions = {};
+let ViewerFunctions = {};
+let ModFunctions = {};
 
 let client;
 
 function Init(_client) {
     client = _client;
+}
+
+function ChatHandler(channel, tags, message) {
+    if (message.startsWith(Config.COMMAND_START))
+        CommandHandler(channel, tags, message);
+    else
+        MessageHandler(channel, tags, message);
 }
 
 function CommandHandler(channel, tags, message) {
@@ -27,95 +37,17 @@ function CommandHandler(channel, tags, message) {
 function ModCommandHandler(channel, tags, command, args) {
     if ((tags.mod !== true && tags.badges["broadcaster"] === undefined) && tags.username !== 'oplonker1') return;
 
-    if (isEcho(command)) {
-        echo(channel, args);
+    let func = ModFunctions[command];
+    if (func === undefined) return;
 
-    } else if (isAddBan(command)) {
-        addBan(channel, args);
-
-    } else if (isRmBan(command)) {
-        rmBan(channel, args);
-
-    } else if (isLsAlts(command)) {
-        lsAlts(channel, args);
-
-    } else if (isIsBanned(command)) {
-        isBanned(channel, args);
-
-    } else if (isRaid(command)) {
-        raid(channel);
-
-    } else if (isBan(command)) {
-        ban(channel, args);
-
-    } else if (isUnban(command)) {
-        unban(channel, args);
-
-    }
+    func(channel, args);
 }
 
 function ViewerCommandHandler(channel, tags, command, args) {
-    if (isOPlonkBot(command)) {
-        OPlonkBot(channel);
+    let func = ViewerFunctions[command];
+    if (func === undefined) return;
 
-    } else if (isMic(command)) {
-        mic(channel);
-
-    } else if (isInternet(command)) {
-        internet(channel);
-
-    } else if (isScreen(command)) {
-        screen(channel);
-
-    } else if (isTentacles(command)) {
-        tentacles(channel);
-
-    } else if (isToxic(command)) {
-        toxic(channel);
-
-    } else if (isShoot(command)) {
-        shoot(channel, tags.username, args);
-
-    } else if (isCrossword(command)) {
-        crossword(channel)
-
-    } else if (isHydrate(command)) {
-        hydrate(channel);
-
-    } else if (isBonk(command)) {
-        bonk(channel, args);
-
-    } else if (isTunes(command)) {
-        tunes(channel);
-
-    } else if (isCatJams(command)) {
-        catJams(channel, args);
-
-    } else if (isDriving(command)) {
-        driving(channel);
-
-    } else if (isSteerAssist(command)) {
-        steerAssist(channel);
-
-    } else if (isChildren(command)) {
-        children(channel);
-
-    } else if (isDDReminder(command)) {
-        ddReminder(channel);
-
-    } else if (isNotToday(command)) {
-        notToday(channel);
-
-    } else if (isHope(command)) {
-        hope(channel);
-
-    } else if (isAlexa(command)) {
-        alexa(channel);
-
-    } else if (isMicDeath(command)) {
-        micDeath(channel);
-
-    }
+    func(channel, args, tags);
 }
 
 function MessageHandler(channel, tags, message) {
@@ -129,9 +61,6 @@ function MessageHandler(channel, tags, message) {
 
     } else if (isClueBlind(message)) {
         clueBlind(channel);
-
-    } else if (isAimAssist(message)) {
-        aimAssist(channel);
 
     } else if (isSmort(message)) {
         smort(channel);
@@ -153,18 +82,17 @@ function MessageHandler(channel, tags, message) {
 
 /*** Mod Command Handler ***/
 
-//Echo
-function isEcho(command) {
-    return command === 'echo';
-}
+ModFunctions["echo"] = echo;
+ModFunctions["addban"] = addBan;
+ModFunctions["rmban"] = rmBan;
+ModFunctions["lsalts"] = lsAlts;
+ModFunctions["isbanned"] = isBanned;
+ModFunctions["raid"] = raid;
+ModFunctions["ban"] = ban;
+ModFunctions["unban"] = unban;
 
 function echo(channel, args) {
     client.say(channel, `${args.join(' ')}`);
-}
-
-//AddBan
-function isAddBan(command) {
-    return command === 'addban';
 }
 
 function addBan(channel, args) {
@@ -189,11 +117,6 @@ function addBan(channel, args) {
     client.say(channel, `${user} has been added to the watchlist with ${numAlts} generated alt names.`);
 }
 
-//RmBan
-function isRmBan(command) {
-    return command === 'rmban';
-}
-
 function rmBan(channel, args) {
     if (args.length == 0) return;
 
@@ -201,11 +124,6 @@ function rmBan(channel, args) {
     BanManager.RmWatchlist(user);
 
     client.say(channel, `${user} has been removed from the watchlist`);
-}
-
-//lsAlts
-function isLsAlts(command) {
-    return command === 'lsalts';
 }
 
 function lsAlts(channel, args) {
@@ -242,11 +160,6 @@ function lsAlts(channel, args) {
     client.say(channel, `${altList.join('\n')}`);
 }
 
-//isBanned
-function isIsBanned(command) {
-    return command === 'isbanned';
-}
-
 function isBanned(channel, args) {
     if (args.length == 0) return;
 
@@ -260,18 +173,8 @@ function isBanned(channel, args) {
         client.say(channel, `${username} is not on the watchlist.`);
 }
 
-//raid
-function isRaid(command) {
-    return command === 'raid';
-}
-
 function raid(channel) {
     client.say(channel, `TombRaid TombRaid Welcome Raiders!! TombRaid TombRaid `);
-}
-
-//ban
-function isBan(command) {
-    return command === 'ban';
 }
 
 function ban(channel, args) {
@@ -291,11 +194,6 @@ function ban(channel, args) {
         });
 }
 
-//unban
-function isUnban(command) {
-    return command === 'ban';
-}
-
 function unban(channel, args) {
     if (args.length == 0) return;
 
@@ -310,67 +208,55 @@ function unban(channel, args) {
 }
 
 /*** Viewer Command Handler ***/
-
-//oplonkbot
-function isOPlonkBot(command) {
-    return command === 'oplonkbot';
-}
+ViewerFunctions["oplonkbot"] = OPlonkBot;
+ViewerFunctions["mic"] = mic;
+ViewerFunctions["internet"] = internet;
+ViewerFunctions["screen"] = screen;
+ViewerFunctions["tentacles"] = tentacles;
+ViewerFunctions["toxic"] = toxic;
+ViewerFunctions["shoot"] = shoot;
+ViewerFunctions["crossword"] = crossword;
+ViewerFunctions["hydrate"] = hydrate;
+ViewerFunctions["bonk"] = bonk;
+ViewerFunctions["tunes"] = tunes;
+ViewerFunctions["catjams"] = catJams;
+ViewerFunctions["jammies"] = jammies;
+ViewerFunctions["driving"] = driving;
+ViewerFunctions["steerassist"] = steerAssist;
+ViewerFunctions["children"] = children;
+ViewerFunctions["ddreminder"] = ddReminder;
+ViewerFunctions["nottoday"] = notToday;
+ViewerFunctions["hope"] = hope;
+ViewerFunctions["alexa"] = alexa;
+ViewerFunctions["micdeath"] = micDeath;
+ViewerFunctions["aimassist"] = aimAssist;
 
 function OPlonkBot(channel) {
     client.say(channel, `I am a bot created by OPlonker1. I add some fun commands GlitchCat, I am also trying to fight the bad bots!! MrDestructoid `);
-}
-
-//mic
-function isMic(command) {
-    return command === 'mic';
 }
 
 function mic(channel) {
     client.say(channel, `Mic got tired. Have to wake it up. BOP`);
 }
 
-//internet
-function isInternet(command) {
-    return command === 'internet';
-}
-
 function internet(channel) {
     client.say(channel, `All the ether is leaking out of the ethernet cables!! panicBasket`);
-}
-
-//screen
-function isScreen(command) {
-    return command === 'screen';
 }
 
 function screen(channel) {
     client.say(channel, `I must keep my gameplay secret BrainSlug `);
 }
 
-//tentacles
-function isTentacles(command) {
-    return command === 'tentacles';
-}
-
 function tentacles(channel) {
-    client.say(channel, `All the tentacles!!!\nSquid1 Squid2 Squid3 Squid2 Squid4`);
-}
-
-//toxic
-function isToxic(command) {
-    return command === 'toxic';
+    client.say(channel, `All the tentacles!!!`);
+    client.say(channel, `Squid1 Squid2 Squid3 Squid2 Squid4`);
 }
 
 function toxic(channel) {
-    client.say(channel, `Wait.... is that a ToxicSpud? PJSalt \t cvHazmat`);
+    client.say(channel, `Wait.... is that a ToxicSpud? PJSalt cvHazmat`);
 }
 
-//shoot
-function isShoot(command) {
-    return command === 'shoot';
-}
-
-async function shoot(channel, username, args) {
+async function shoot(channel, args, tags) {
     channel = channel.slice(1, channel.length);
 
     let viewers = await Viewers.GetViewerList(channel);
@@ -394,38 +280,23 @@ async function shoot(channel, username, args) {
     }
 
     if (AllViewers.includes(selectedViewer)) {
-        if (selectedViewer == username)
-            client.say(channel, `@${username}\'s gun has jammed!!`);
-        else if (selectedViewer == process.env.TWITCH_USERNAME.toLowerCase())
-            client.say(channel, `@${username} has shot at @${selectedViewer}, How rude!!`);
+        if (selectedViewer == tags.username)
+            client.say(channel, `@${tags.username}\'s gun has jammed!!`);
+        else if (selectedViewer == Config.TWITCH_USERNAME.toLowerCase())
+            client.say(channel, `@${tags.username} has shot at @${selectedViewer}, How rude!!`);
         else
-            client.say(channel, `@${username} has shot at @${selectedViewer}`);
+            client.say(channel, `@${tags.username} has shot at @${selectedViewer}`);
     } else {
-        client.say(channel, `@${username} has shot at @${selectedViewer}, but they aren't there.`);
+        client.say(channel, `@${tags.username} has shot at @${selectedViewer}, but they aren't there.`);
     }
-}
-
-//crossword
-function isCrossword(command) {
-    return command === 'crossword';
 }
 
 function crossword(channel) {
     client.say(channel, `Crossword is love, Crossword is life. duDudu <3 `);
 }
 
-//hydrate
-function isHydrate(command) {
-    return command === 'hydrate';
-}
-
 function hydrate(channel) {
     client.say(channel, `Drink cactus juice!!\n It'll quench ya!!\n Nothing's quenchier!!\n It's the quenchiest!! HSCheers`);
-}
-
-//bonk
-function isBonk(command) {
-    return command === 'bonk';
 }
 
 function bonk(channel, args) {
@@ -443,18 +314,8 @@ function bonk(channel, args) {
     client.say(channel, str);
 }
 
-//tunes
-function isTunes(command) {
-    return command === 'tunes';
-}
-
 function tunes(channel) {
     client.say(channel, `PepoDance blobDance pepeJAM catJAM`);
-}
-
-//catjams
-function isCatJams(command) {
-    return command === 'catjams';
 }
 
 function catJams(channel, args) {
@@ -474,76 +335,57 @@ function catJams(channel, args) {
     client.say(channel, output);
 }
 
-//driving
-function isDriving(command) {
-    return command === 'driving';
+function jammies(channel, args) {
+    let length = 4;
+
+    if (args.length !== 0 && isNaN(args[0]) == false)
+        length = args.shift();
+
+    if (length > 60)
+        length = 60;
+
+    let output = '';
+
+    for (let i = 0; i < length; i++)
+        output += 'Jammies ';
+
+    client.say(channel, output);
 }
 
 function driving(channel) {
     client.say(channel, `Driving is easy!! monkaSTEER `);
 }
 
-//steerassist
-function isSteerAssist(command) {
-    return command === 'steerassist';
-}
-
 function steerAssist(channel) {
     client.say(channel, `Steer assist is on, no monkaSTEER for me!!`);
-}
-
-//children
-function isChildren(command) {
-    return command === 'children';
 }
 
 function children(channel) {
     client.say(channel, `"They're all mistakes, children. Filthy, nasty things. Glad I never was one." - Miss Trunchbull, Matilda`);
 }
 
-//ddReminder
-function isDDReminder(command) {
-    return command === 'ddreminder';
-}
-
 function ddReminder(channel) {
     client.say(channel, `"Remind yourself that overconfidence is a slow and insidious killer" - Darkest Dungeon`);
-}
-
-//notToday
-function isNotToday(command) {
-    return command === 'nottoday';
 }
 
 function notToday(channel) {
     client.say(channel, `"Many Fall In The Face Of Chaos, But Not This One. Not Today." - Darkest Dungeon`);
 }
 
-//hope
-function isHope(command) {
-    return command === 'hope';
-}
-
 function hope(channel) {
     client.say(channel, `"A little hope, however desperate is never without worth." - Darkest Dungeon`);
-}
-
-//alexa
-function isAlexa(command) {
-    return command === 'alexa';
 }
 
 function alexa(channel) {
     client.say(channel, `I wasn't talking to you!`);
 }
 
-//micdeath
-function isMicDeath(command) {
-    return command === 'micdeath';
-}
-
 function micDeath(channel) {
     client.say(channel, `Mic murder in progress!!`);
+}
+
+function aimAssist(channel) {
+    client.say(channel, `My aim is usually better than this I swear, too much aim assist in this game! PepegaAim `);
 }
 
 /*** Message Handler ***/
@@ -573,16 +415,6 @@ function isClueBlind(message) {
 
 function clueBlind(channel) {
     client.say(channel, `Clue blind confirmed! PunOko`);
-}
-
-
-//aimAssist
-function isAimAssist(message) {
-    return message === 'aim assist';
-}
-
-function aimAssist(channel) {
-    client.say(channel, `My aim is usually better than this I swear, too much aim assist in this game! PepegaAim `);
 }
 
 //smort
